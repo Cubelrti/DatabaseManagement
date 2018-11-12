6b9c67026c18b8c5b824c2e4b8f0bef688dbae4d
Cubelrti <tankrity@gmail.com>
2018-11-12 23:04:57 +0800

    Initial commit

---
+++ DatabaseManagement/Core/Expr.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManagement.Core
+{
+    class Expr
+    {
+        public enum ExprType
+        {
+            CREATE,
+            DROP,
+            UPDATE,
+            SELECT,
+            INSERT,
+            DELETE,
+        }
+        public ExprType type;
+        private string lhs;
+        private string rhs;
+        
+        public void compile(ExprType type, string lhs, string rhs)
+        {
+            Expr expr = new Expr();
+            expr.lhs = lhs;
+            expr.rhs = rhs;
+            expr.type = type;
+        }
+        private void excute()
+        {
+            // TODO excute this expression
+        }
+        private Expr()
+        {
+        }
+
+    }
+}
