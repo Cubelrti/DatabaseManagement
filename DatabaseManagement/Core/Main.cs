36c71bbd5fd714d44b05374ab16fd300f8164d9e
Cubelrti <tankrity@gmail.com>
2018-11-13 10:07:43 +0800

    Update DatabaseManagement/Core/Main.cs

--- DatabaseManagement/Core/Main.cs
+++ DatabaseManagement/Core/Main.cs
@@ -8,54 +8,6 @@ namespace DatabaseManagement.Core
 {
     class Main : Intefaces.Database
     {
-        public void CreateDatabase(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void CreateTable(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DropDatabases(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DropTable(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Insert(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Select(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowDatabases(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ShowTables(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Expr expr)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
